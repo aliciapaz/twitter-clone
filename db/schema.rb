68019d0a33e3f98b0603a96d3136aa3b2f22172e
aliciapaz <aliciapazrojas@gmail.com>
2021-02-23 18:02:42 -0300

    customize views for password, sign up, sign in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_201453) do
+ActiveRecord::Schema.define(version: 2021_02_23_203437) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
@@ -31,8 +31,11 @@ ActiveRecord::Schema.define(version: 2021_02_23_201453) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
